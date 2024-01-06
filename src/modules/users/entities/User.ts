8828965ccb8872ea3959a483ce2a24cb3a7a3de7
catito <catitosantosp@gmail.com>
2024-01-06 17:26:25 -0300

    feat: add user entity

---
+++ src/modules/users/entities/User.ts
@@ -0,0 +1,47 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
+import {v4 as uuidV4} from "uuid";
+
+@Entity("users")
+class User {
+
+    @PrimaryColumn({
+        type:"uuid"
+    })
+    id: string;
+
+    @Column({
+        type:"varchar",
+        length: 80
+    })
+    name: string;
+
+    @Column({
+        type:"varchar",
+        unique: true
+    })
+    email: string;
+
+    @Column({
+        type:"varchar",
+        length: 20
+    })
+    password: string;
+
+    @CreateDateColumn({
+        name:"createdat"
+    })
+    createdAt: Date;
+
+    @UpdateDateColumn({
+        name:"updatedat"
+    })
+    updatedAt: Date;
+
+    constructor() {
+        if(!this.id) {
+            this.id = uuidV4();
+        }
+    }
+}
+
+export { User }
